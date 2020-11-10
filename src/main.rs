6c1bbc5fce91576422d55b42200cb20055e162f0
Jack Vine <jackv949@gmail.com>
2020-11-11 08:15:32 +1030

    Completed Bevy into tutorial

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,39 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
+}
+
+struct Person;
+struct Name(String);
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn((Person, Name("Jenk".to_string())))
+        .spawn((Person, Name("Brian".to_string())))
+        .spawn((Person, Name("Brinty".to_string())))
+        .spawn((Person, Name("Jams".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<(&Person, &Name)>) {
+    
+    timer.0.tick(time.delta_seconds);
+
+    if timer.0.finished {
+        for (_person, name) in query.iter() {
+            println!("hello {0}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
 }
